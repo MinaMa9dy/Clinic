ea3e66ca8c0235ff13f72407e87444110bc39ac3
mina <mandymina789@gmil.com>
2026-02-26 02:00:48 +0200

    api changed

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:7219/api',
+  baseURL: 'https://myclinicbe.runasp.net/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
